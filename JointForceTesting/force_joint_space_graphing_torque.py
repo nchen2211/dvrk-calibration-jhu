5bf5d1885437b1189ba3f691dcebe8d71fded26a
nchen2211 <nchrysilla@gmail.com>
2017-07-26 19:10:48 -0400

    changed some ranges

--- JointForceTesting/force_joint_space_graphing_torque.py
+++ JointForceTesting/force_joint_space_graphing_torque.py
@@ -37,30 +37,30 @@ def get_first_last_index(reader, depth_index, startIndex, endIndex):
 def run():
 
     data_files = [] 
-    Joint1Data = PdfPages('Joint1Data_t.pdf')
-    Joint1TorqueOffsetSlope = PdfPages('Joint1TorqueOffsetSlope.pdf')
-    Joint2Data = PdfPages('Joint2Data_t.pdf')
-    Joint2TorqueOffsetSlope = PdfPages('Joint2TorqueOffsetSlope.pdf')
+    Joint1Data = PdfPages('Joint1_TorqueOffset.pdf')
+    Joint1TorqueOffsetSlope = PdfPages('Joint1_TorqueOffsetSlope.pdf')
+    Joint2Data = PdfPages('Joint2_TorqueOffset.pdf')
+    Joint2TorqueOffsetSlope = PdfPages('Joint2_TorqueOffsetSlope.pdf')
 
     # finding the relevant csv files in the directory
     for document in os.listdir("ForceTestingDataJointSpace"):
-        if document.startswith("force_joint_space_torque_offsets"):
+        if document.startswith("torque_offsets_output"):
             if 'joint_0' in document:
                 data_files.insert(0,document)
             elif 'joint_1' in document:
                 data_files.insert(1,document)
 
-    # for graphing the slope
-    all_depth_slopes = []
-    all_depth = []
-
     # process each relevant file
     for doc_index in range(len(data_files)):
-        # print "document ", doc_index
+        print "document ", doc_index
         
         joint_efforts = []
         joint_positions = []
         joint_depth = []   
+
+         # for graphing the slope
+        all_depth_slopes = []
+        all_depth = []
 
         current_file = (str(data_files[doc_index]))
         reader = list(csv.reader(open('ForceTestingDataJointSpace/' + current_file,"rb"), delimiter=','))
@@ -74,9 +74,6 @@ def run():
 			# determine first and end index for each depth
             startIndex, endIndex = get_first_last_index(reader, depth_index, startIndex, endIndex)
 
-            # store depths for slope graph
-            all_depth.append(float(reader[startIndex][1]))
-
             # storing data into arrays       
             for row in range(endIndex, startIndex-1, -1):
             	joint_efforts.append(float(reader[row][3]))
@@ -85,21 +82,24 @@ def run():
             # reverse back to normal order
             joint_efforts.reverse() 
             joint_positions.reverse() 
-           
-            # plotting data of joint x 
+            
+            # getting the slope
             slope, offset = numpy.polyfit(joint_positions, joint_efforts, 1)
+            print slope
             all_depth_slopes.append(slope)
+            all_depth.append(float(reader[startIndex][1]))
+
             plt.plot(joint_positions, joint_efforts, '-', label = ("Depth " + str(depth_index)))
             x1, x2, y1, y2 = plt.axis()
-            plt.axis((x1,x2,-0.2,0.4))
+            plt.axis((x1,x2,-0.2,0.3))
 
             # empty array elements
             joint_efforts[:] = []
             joint_positions[:] = []
             joint_depth[:] = []
 
-        plt.xlabel("Joint position, radians")
-        plt.ylabel("Joint effort, N-m")
+        plt.xlabel("Joint position, radians", size=18)
+        plt.ylabel("Joint effort, N-m", size=18)
         plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
            ncol=5, mode="expand", borderaxespad=0.)
 
@@ -110,19 +110,23 @@ def run():
         plt.show()
 
         # plot slopes for each depth
-        # plt.plot(xaxis, ((A*(xaxis**3)) + (B*(xaxis**2)) + (C*xaxis) + D), '-')
         if (doc_index == 0):
             xaxis = numpy.arange(0.0, 0.7, 0.15)
             A,B = numpy.polyfit(all_depth, all_depth_slopes, 1)
             plt.plot(xaxis, ((A*xaxis) + B), '-')
+            print "A=", A, "B=", B
         elif (doc_index == 1):
             xaxis = numpy.arange(0.0, 0.4, 0.15)
-            A,B,C = numpy.polyfit(all_depth, all_depth_slopes, 2)
-            plt.plot(xaxis, ((A*(xaxis**2)) + (B*xaxis) + C), '-')
+            A,B = numpy.polyfit(all_depth, all_depth_slopes, 1)
+            # plt.plot(xaxis, ((A*(xaxis**2)) + (B*xaxis) + C), '-')
+            plt.plot(xaxis, ((A*xaxis) + B), '-')
+            print "A1=", A, "B1=", B
 
         plt.plot(all_depth, all_depth_slopes, '.')
-        plt.xlabel("Depth, m")
-        plt.ylabel("Torque Offset, N-m/radians")
+        x1, x2, y1, y2 = plt.axis()
+        plt.axis((x1,0.3,0.0,0.3))
+        plt.xlabel("Depth, m", size=18)
+        plt.ylabel("Torque Offset, N-m/radians", size=18)
 
         if doc_index == 0:
             plt.savefig(Joint1TorqueOffsetSlope, format = 'pdf')
