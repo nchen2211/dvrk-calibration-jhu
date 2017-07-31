66b63605e56b5187eacc23a5625160a00a50107e
nchen2211 <nchrysilla@gmail.com>
2017-07-31 17:57:14 -0400

    write plate cartesian added

--- JointForceTesting/force_joint_space_graph_compensation.py
+++ JointForceTesting/force_joint_space_graph_compensation.py
@@ -15,6 +15,116 @@ import datetime
 MAX_EFFORT = 1.2 
 POINT = "2"
 ANGLE = "90"
+LETTER = 'A'
+
+def write_cartesian():
+
+    list_compensated_x_avg = []
+    list_compensated_y_avg = []
+    list_compensated_z_avg = []
+    list_encoder_x_avg = []
+    list_encoder_y_avg = []
+    list_encoder_z_avg = []
+    list_point_compensated_effort = []
+    list_point_encoder_effort = []
+    list_point = []
+    
+    for directory in range(1, 13):
+
+        compensated_x = 0.0
+        compensated_y = 0.0
+        compensated_z = 0.0
+        encoder_x = 0.0
+        encoder_y = 0.0
+        encoder_z = 0.0
+        compensated_joint_effort = 0.0
+        encoder_joint_effort = 0.0
+        bag_files_compensated = []
+        bag_files_encoder = []
+
+        if (directory == 10):
+            directory = chr(ord(LETTER))
+
+        elif (directory > 10):
+            directory = chr(ord(prev_dir) + 1)
+
+        prev_dir = directory
+        list_point.append(str(directory))
+
+        target = "CorrectionData/DistanceMeasurement/Point_" + str(directory) + "/"
+        for document in os.listdir(target):
+            if (document.startswith("_dvrk_PSM3_compensated_position")):
+                bag_files_compensated.append(document)
+            elif (document.startswith("_dvrk_PSM3_position")):
+                bag_files_encoder.append(document)
+            elif (document.startswith("_dvrk_PSM3_compensated_state")):
+                bag_files_compensated.append(document)
+            elif (document.startswith("_dvrk_PSM3_state")):
+                bag_files_encoder.append(document)
+
+        num_data = 0
+        for document in range(len(bag_files_compensated)):
+            reader = list(csv.reader(open(target + bag_files_compensated[document],"rb"), delimiter=','))
+            for row in range(1, len(reader)): 
+                if (document == 0):
+                    compensated_x += (float(reader[row][4]))
+                    compensated_y += (float(reader[row][5]))
+                    compensated_z += (float(reader[row][6])) 
+                    num_data = len(reader)          
+                elif (document == 1):
+                    compensated_joint_effort += (float(reader[row][23]))
+
+        avg_compensated_x = compensated_x / num_data
+        avg_compensated_y = compensated_y / num_data
+        avg_compensated_z = compensated_z / num_data
+        avg_compensated_effort = compensated_joint_effort /num_data
+
+        for document in range(len(bag_files_encoder)):
+            reader = list(csv.reader(open(target + bag_files_encoder[document],"rb"), delimiter=','))
+            for row in range(1, len(reader)): 
+                if (document == 0):
+                    encoder_x += (float(reader[row][4]))
+                    encoder_y += (float(reader[row][5]))
+                    encoder_z += (float(reader[row][6]))
+                    num_data = len(reader)
+                elif (document == 1):
+                    encoder_joint_effort += (float(reader[row][23]))
+
+        avg_encoder_x = encoder_x / num_data
+        avg_encoder_y = encoder_y / num_data
+        avg_encoder_z = encoder_z / num_data
+        avg_encoder_effort = encoder_joint_effort / num_data
+
+        list_compensated_x_avg.append(str(avg_encoder_x))
+        list_compensated_y_avg.append(str(avg_encoder_y))
+        list_compensated_z_avg.append(str(avg_encoder_z))
+        list_point_compensated_effort.append(str(avg_compensated_effort))
+        list_encoder_x_avg.append(str(avg_encoder_x))
+        list_encoder_y_avg.append(str(avg_encoder_y))
+        list_encoder_z_avg.append(str(avg_encoder_z))
+        list_point_encoder_effort.append(str(avg_encoder_effort))
+
+    target = "CorrectionData/DistanceMeasurement/"
+    outputFile = target + "/points_cartesian_compensated.txt"  
+    print "\n Values will be saved in:", outputFile
+    f = open(outputFile, 'wr+') # wb is used in python to write on file (write binary)
+    f.write('# dVRK compensated coordinate for aluminum test plate\n')
+    f.write('# point        x        y        z        effort\n')
+
+    for row in range(len(list_compensated_x_avg)): 
+        item = list_point[row] + "    " + list_compensated_x_avg[row] + "    " + list_compensated_y_avg[row] + "    " + list_compensated_z_avg[row] + "    " + list_point_compensated_effort[row] 
+        line = f.writelines(item + '\n')
+
+    target = "CorrectionData/DistanceMeasurement/"
+    outputFile = target + "/points_cartesian_encoder.txt"  
+    print "\n Values will be saved in:", outputFile
+    f = open(outputFile, 'wr+') # wb is used in python to write on file (write binary)
+    f.write('# dVRK encoder coordinate for aluminum test plate\n')
+    f.write('# point    x    y    z    effort\n')
+
+    for row in range(len(list_encoder_x_avg)):
+        item = list_point[row] + "    " + list_encoder_x_avg[row] + "    " + list_encoder_y_avg[row] + "    " + list_encoder_z_avg[row] + "    " + list_point_encoder_effort[row] 
+        line = f.writelines(item + '\n')
 
 def write_to_file():
     
@@ -91,8 +201,10 @@ def run():
 
     Joint1Comparison.close()
 
-option = int(input("Enter:\n[1] for plotting compensation comparison graph \n[2] for generating comparison file\n"))
+option = int(input("Enter:\n[1] for plotting compensation comparison graph \n[2] for generating comparison file\n[3] for generating cartesian of plate distance"))
 if (option == 1):
     run()
 elif (option == 2):
     write_to_file()
+elif (option == 3):
+    write_cartesian()
